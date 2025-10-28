a9ebaec47c7cbc86090d220475d462beceb6f289
garsonj <110767551+garsonj@users.noreply.github.com>
2025-10-28 14:38:20 +0300

    use postgresql as default db

--- laravel-backend/config/database.php
+++ laravel-backend/config/database.php
@@ -15,6 +15,8 @@ return [
     |
     */
 
+    // use postgresql as default database
+
     'default' => env('DB_CONNECTION', 'pgsql'),
 
 
@@ -77,6 +79,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+
         ],
 
         'sqlsrv' => [
